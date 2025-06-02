735736e230c9f6471bde6c5b7b2f000865d0f953
Hunter1995 <zenyalyt@gmail.com>
2025-06-02 13:42:20 +0200

    Added Items component(instead of bd connection) and some new styles to index.css

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,13 +1,50 @@
+import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Items from "./components/Items";
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Footer />
-    </div>
+class App extends React.Component {
+  /* Array instead of database */
+  constructor(props){
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Chair',
+          img: '',
+          desc: 'Lorem ipsum dolor sit amet',
+          category: 'chairs',
+          price: '49.9'
+        },
+        {
+          id: 2,
+          title: 'Table',
+          img: '',
+          desc: 'Lorem ipsum dolor sit amet',
+          category: 'tables',
+          price: '100.9'
+        },
+        {
+          id: 3,
+          title: 'Lamp',
+          img: '',
+          desc: 'Lorem ipsum dolor sit amet',
+          category: 'lamps',
+          price: '2.9'
+        }
+      ]
+    }
+  }
+  render(){
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items} />
+        <Footer />
+      </div>
   );
+  }
 }
 
 export default App;
--- shop/src/components/Header.js
+++ shop/src/components/Header.js
@@ -5,6 +5,11 @@ export default function Header() {
     <header>
         <div>
             <span className='logo'>House Staff</span>
+            <ul className='nav'>
+                <li>About us</li>
+                <li>Contacts</li>
+                <li>Cabinet</li>
+            </ul>
         </div>
         <div className='presentation'></div>
     </header>

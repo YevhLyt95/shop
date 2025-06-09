1f51eaaa91b05c3432730c06c1ede1a39ddb0c0d
Hunter1995 <zenyalyt@gmail.com>
2025-06-09 22:01:41 +0200

    Added possibility to add wears to cart. Now it is impossible to add double of one item. As my own idea(not code from course) I will add x2 if user adds double

--- shop/src/App.js
+++ shop/src/App.js
@@ -5,9 +5,10 @@ import Items from "./components/Items";
 
 class App extends React.Component {
   /* Array instead of database */
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
+      orders: [],
       items: [
         {
           id: 1,
@@ -35,15 +36,28 @@ class App extends React.Component {
         }
       ]
     }
+    this.addToOrder = this.addToOrder.bind(this); //in order to use state from addToOrder
   }
-  render(){
+  render() {
     return (
       <div className="wrapper">
-        <Header />
-        <Items items={this.state.items} />
+        <Header orders={this.state.orders} />
+        <Items items={this.state.items} onAdd={this.addToOrder} />
         <Footer />
       </div>
-  );
+    );
+  }
+
+  addToOrder(item) {
+    let isInArray = false;
+    this.state.orders.forEach(el => {
+      if (el.id === item.id)
+        isInArray = true;
+    });
+    if (!isInArray) {
+      this.setState({ orders: [...this.state.orders, item] });
+    }
+
   }
 }
 
--- shop/src/components/Header.js
+++ shop/src/components/Header.js
@@ -1,26 +1,48 @@
 import React, { useState } from 'react'
-import {FaShoppingCart} from 'react-icons/fa'
+import { FaShoppingCart } from 'react-icons/fa'
+import Order from './Order';
 
-export default function Header() {
-let [cartOpen, setCartOpen] = useState(false);
+const showOrders = (props) => {
+  return (
+    <div>
+      {props.orders.map(el => (
+        <Order key={el.id} item={el} />
+      ))}
+    </div>
+  );
+}
+
+const showNothing = () => {
+  return (
+    <div className='empty'>
+      <h2>Nothing exist here yet</h2>
+    </div>
+  );
+}
+
+export default function Header(props) {
+  let [cartOpen, setCartOpen] = useState(false);
 
   return (
     <header>
-        <div>
-            <span className='logo'>House Staff</span>
-            <ul className='nav'>
-                <li>About us</li>
-                <li>Contacts</li>
-                <li>Cabinet</li>
-            </ul>
-            <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)} 
-              className={`shop-cart-button ${cartOpen && 'active'}`} />
+      <div>
+        <span className='logo'>House Staff</span>
+        <ul className='nav'>
+          <li>About us</li>
+          <li>Contacts</li>
+          <li>Cabinet</li>
+        </ul>
+        <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)}
+          className={`shop-cart-button ${cartOpen && 'active'}`} />
 
-              {cartOpen && (
-                <div className='shop-cart'></div>
-              )}
-        </div>
-        <div className='presentation'></div>
+        {cartOpen && (
+          <div className='shop-cart'>
+            {props.orders.length > 0 ?
+              showOrders(props) : showNothing()}
+          </div>
+        )}
+      </div>
+      <div className='presentation'></div>
     </header>
   )
 }
